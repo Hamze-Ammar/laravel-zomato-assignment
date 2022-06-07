db9cd946023540c0b576802b42cbbfca96cc6cbf
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-07 19:56:24 +0300

    Added changes (testing push from vscode)

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function signUp(Request $request){
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = hash("sha256", $request->password);
+        $user->type = 0;
+
+
+        $user->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
+}
